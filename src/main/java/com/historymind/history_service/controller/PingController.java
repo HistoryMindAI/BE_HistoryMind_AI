7997d292bf93c33537836f39d804a769b36405aa
L1nkinPark <hieuvdfs91539@fpt.edu.vn>
2026-02-09 03:49:10 +0700

    Debug: Add Ping endpoint, enable logs, upgrade Spring Boot 3.2.2

---
+++ src/main/java/com/historymind/history_service/controller/PingController.java
@@ -0,0 +1,14 @@
+package com.historymind.history_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public Mono<String> ping() {
+        return Mono.just("pong");
+    }
+}
